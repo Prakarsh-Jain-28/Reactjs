66985083e942bdb43b39c76491f3630148169612
Prakarsh Jain <prakarshj51@gmail.com>
2026-08-07 14:29:15 +0530

    Learing to create a Componenet in Lec 3

---
+++ Lec 3/src/App.jsx
@@ -0,0 +1,14 @@
+import Ipl from "./ipl"
+
+function App() {
+
+  return (
+    <>
+      <h1>IPL 2026</h1>
+      <Ipl />
+      <h4>Finals - Ahemdabad</h4>
+    </>
+  )
+}
+
+export default App
---
+++ Lec 3/src/ipl.jsx
@@ -0,0 +1,12 @@
+function Ipl ()
+{
+    return (
+        <>
+            <h2>RCB Champions IPL 2026</h2>
+            <h3>Virat Kohli MOM of Finals</h3>
+            <h4>Rcb Beat Gt by 5 Wickets</h4>
+        </>
+    )
+}
+
+export default Ipl
